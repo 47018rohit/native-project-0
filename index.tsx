e2eaab9c969c34f3aa21ff5e416b96a6c0ef7f18
rohit prakash <rohit.p@celcius.in>
2025-02-20 16:36:04 +0530

    dev: first commit project setup

--- index.tsx
+++ index.tsx
@@ -1,12 +1,12 @@
 import { registerRootComponent } from "expo";
-import AppNavigator from "./src/app/navigation/AppNavigator";
 import { FC } from "react";
 import { PaperProvider } from "react-native-paper";
+import App from "./App";
 
-const App: FC = () => (
+const Main: FC = () => (
   <PaperProvider>
-    <AppNavigator />
+    <App />
   </PaperProvider>
 );
 
-registerRootComponent(App);
+registerRootComponent(Main);
--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { registerRootComponent } from "expo";
-import AppNavigator from "./src/app/navigation/AppNavigator";
-
-const App = () => <AppNavigator />;
-
-registerRootComponent(App);
